76993201d0734f8d3b94f75e473807c75bea8ca4
Тимофей Карклин <timkar164@gmail.com>
2021-06-19 23:23:55 +0300

    2323

--- src/app/metrics/metrics.component.ts
+++ src/app/metrics/metrics.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpErrorResponse } from '@angular/common/http/src/response';
+
 
 @Component({
   selector: 'app-metrics',
@@ -10,7 +10,7 @@ import { HttpErrorResponse } from '@angular/common/http/src/response';
 export class MetricsComponent implements OnInit {
   public fileToUpload: File = null;
   public isFile = false;
-  public isPlay = false;
+  public isPlay = true;
   constructor(private httpService: HttpClient) { }
 
 
@@ -20,22 +20,26 @@ export class MetricsComponent implements OnInit {
 
   getFileDetails (e) {
     console.log (e.target.files);
+
     for (var i = 0; i < e.target.files.length; i++) {
       this.myFiles.push(e.target.files[i]);
     }
   }
   uploadFiles () {
+    this.isPlay=false;
     const frmData = new FormData();
     console.log(frmData);
     for (var i = 0; i < this.myFiles.length; i++) {
       frmData.append("fileUpload", this.myFiles[i]);
     }
 
-    this.httpService.post('http://127.0.0.1:5000/files', frmData).subscribe(
+    this.httpService.post('https://flask-api-miriteam.herokuapp.com/files', frmData).subscribe(
       data => {
         // SHOW A MESSAGE RECEIVED FROM THE WEB API.
         this.sMsg = data as string;
         console.log (this.sMsg);
+        this.isFile = true;
+        this.isPlay = true;
       },
 
     );
