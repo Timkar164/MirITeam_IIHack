4fee16af804f249e799f611f61a92e608a2899b1
Тимофей Карклин <timkar164@gmail.com>
2021-06-19 22:00:01 +0300

    2159

--- src/app/metrics/metrics.component.ts
+++ src/app/metrics/metrics.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 @Component({
   selector: 'app-metrics',
@@ -9,9 +11,35 @@ export class MetricsComponent implements OnInit {
   public fileToUpload: File = null;
   public isFile = false;
   public isPlay = false;
-  constructor() { }
+  constructor(private httpService: HttpClient) { }
 
-  ngOnInit() {
+
+  ngOnInit () {  }
+  myFiles:string [] = [];
+  sMsg:string = '';
+
+  getFileDetails (e) {
+    console.log (e.target.files);
+    for (var i = 0; i < e.target.files.length; i++) {
+      this.myFiles.push(e.target.files[i]);
+    }
+  }
+  uploadFiles () {
+    const frmData = new FormData();
+    console.log(frmData);
+    for (var i = 0; i < this.myFiles.length; i++) {
+      frmData.append("fileUpload", this.myFiles[i]);
+    }
+
+    this.httpService.post('http://127.0.0.1:5000/files', frmData).subscribe(
+      data => {
+        // SHOW A MESSAGE RECEIVED FROM THE WEB API.
+        this.sMsg = data as string;
+        console.log (this.sMsg);
+      },
+
+    );
+    this.myFiles =[];
   }
 
   handleFileInput(files: FileList) {
@@ -19,6 +47,8 @@ export class MetricsComponent implements OnInit {
     console.log(this.fileToUpload);
     this.isFile=true;
   }
+
+
 
 
 }
